316f20367e46041d60c589f8f04c153bfce47f04
poohitan <poohitan@gmail.com>
2017-12-26 12:34:28 +0200

    Add compatibility with old trash permalinks

--- routes/trash-posts.js
+++ routes/trash-posts.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const moment = require('moment');
 const models = require('../models');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  models.trashPost.find()
+  let filter = {};
+
+  if (req.query.createdAt) {
+    const createdAt = moment(req.query.createdAt);
+
+    filter = {
+      createdAt: {
+        $gte: createdAt.clone().startOf('second'),
+        $lte: createdAt.clone().endOf('second'),
+      },
+    };
+  }
+
+  models.trashPost.find(filter)
     .then(trashPosts => res.json(trashPosts.map(trashPost => trashPost.serialize())));
 });
 
