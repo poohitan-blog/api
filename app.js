c2baf0b88c1d4aab51383654441060ee80a8794c
poohitan <poohitan@gmail.com>
2017-12-28 14:31:36 +0200

    Add Logger and body-parser

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const HttpStatus = require('http-status-codes');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-const corsHandler = require('./middlewares/cors-handler');
-const queryParser = require('./middlewares/query-parser');
 const config = require('./config').current;
 const connectToDB = require('./utils/connect-to-db');
+const corsHandler = require('./middlewares/cors-handler');
+const queryParser = require('./middlewares/query-parser');
 const routes = require('./routes');
+const Logger = require('./services/logger');
 
 const app = express();
 
-app.use(corsHandler);
-
+app.use(bodyParser.json());
 app.use(queryParser);
+
+app.use(Logger.request);
+app.use(corsHandler);
 
 app.get('/', (req, res) => {
   res.sendStatus(HttpStatus.OK);
@@ -28,6 +33,8 @@ Object.keys(routes).forEach((route) => {
 connectToDB()
   .then(() => {
     app.listen(config.port);
+
+    mongoose.set('debug', Logger.query);
 
     console.log('Listening on port', config.port);
   });
