f2601d572c901599d181ea2ab70cc238890732db
poohitan <poohitan@gmail.com>
2018-01-12 15:53:58 +0200

    Implement endpoint for fetching posts tags

---
+++ routes/tags.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const models = require('../models');
+const errorHandler = require('../middlewares/error-handler');
+
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+  try {
+    const posts = await models.post.find()
+      .select('tags');
+
+    const tags = posts.reduce((result, post) => result.concat(post.tags), [])
+      .filter((tag, index, array) => array.indexOf(tag) === index)
+      .filter(tag => tag);
+
+    res.json(tags);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get('/cloud', async (req, res, next) => {
+  try {
+    const posts = await models.post.find()
+      .select('tags');
+
+    const cloud = posts
+      .reduce((result, post) => post.tags.reduce((innerResult, tag) => {
+        if (!tag) {
+          return innerResult;
+        }
+
+        const postsCount = innerResult[tag];
+
+        if (postsCount) {
+          return Object.assign({}, innerResult, { [tag]: postsCount + 1 });
+        }
+
+        return Object.assign({ [tag]: 1 }, innerResult);
+      }, result), {});
+
+    res.json(cloud);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.use(errorHandler);
+
+module.exports = router;
